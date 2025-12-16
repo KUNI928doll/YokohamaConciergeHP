3954922b271d1a07c33a078a3269d9244de5adf0
KUNI928doll <kuni92vintage8doll@gmail.com>
2025-12-16 20:38:35 +0900

    ContactForm7を入れる

--- page-contact.php
+++ page-contact.php
@@ -33,7 +33,7 @@ get_header();
             <div class="inquiry__inner inner">
                 <?php
                 // Contact Form 7 のフォームを表示
-                echo do_shortcode('[contact-form-7 id="bd13808" title="お問い合わせフォーム"]');
+                echo do_shortcode('[contact-form-7 id="8f7b03f" title="お問い合わせフォーム"]');
                 ?>
             </div>
         </section>
