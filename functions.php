44dea430c934a2a76a69041eaf73c99d2c51399f
KUNI928doll <kuni92vintage8doll@gmail.com>
2026-01-04 17:08:08 +0900

    Make(webhook) 連携コードURL差し替え

--- functions.php
+++ functions.php
@@ -1050,7 +1050,7 @@ add_action('template_redirect', 'yokohama_concierge_handle_stripe_success');
 
 // Make Webhook送信関数
 function yokohama_concierge_send_to_make_webhook($post_id, $form_data) {
-    $webhook_url = 'https://hook.us2.make.com/12f8ezalyvew9ymq7b91tykvalm5dmfy';
+    $webhook_url = 'https://hook.us2.make.com/lgwkco1rbv07y7f9kxizoc65x7ubspw5';
 
     $body = array(
         'reservation_id' => (string)$post_id,
